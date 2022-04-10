8c404d18c04f78605aa4645ede9937bf78a5be9c
Carlos Forero <cefbursatil@gmail.com>
2022-04-10 17:22:05 -0500

    Login, Trading Strategies, Falta Trades

--- AppInvest/forms.py
+++ AppInvest/forms.py
@@ -10,14 +10,39 @@ class TradeForm(forms.Form):
     descripcion = forms.CharField(max_length=140) #Descripción general de la estrategia
 
 class TradingStrategyForm(forms.Form):
-    idStrategy=forms.IntegerField()
     nombre=forms.CharField(max_length=40)
     activo = forms.CharField(max_length=40) #En que activo se aplica la estrategia
     descripcion = forms.CharField(max_length=140) #Descripción general de la estrategia
 
 
 class TradersForm(forms.Form):
-    idTrader=forms.IntegerField()
     nombre= forms.CharField(max_length=30)
     apellido= forms.CharField(max_length=30)
     email= forms.EmailField()
+
+
+#USUARIOs LOGIN
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1', 'password2']
+        # Saca los mensajes de ayuda
+        help_texts = {k:"" for k in fields}
+
+class UserEditForm(UserCreationForm):
+    # Obligatorio
+    email = forms.EmailField(label="Ingrese su email:")
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repetir la contraseña', widget=forms.PasswordInput)
+    last_name = forms.CharField()
+    first_name = forms.CharField()
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2', 'last_name', 'first_name']
+
--- AppInvest/models.py
+++ AppInvest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Traders(models.Model):
     idTrader=models.IntegerField()
     nombre= models.CharField(max_length=30)
@@ -8,11 +9,10 @@ class Traders(models.Model):
         return f"Nombre: {self.nombre} - Apellido {self.apellido} - E-Mail {self.email}"
 
 class TradingStrategies(models.Model):
-    idStrategy=models.IntegerField()
     nombre=models.CharField(max_length=40)
     activo = models.CharField(max_length=40) #En que activo se aplica la estrategia
     descripcion = models.CharField(max_length=140) #Descripción general de la estrategia
-    trader=models.ForeignKey(Traders, on_delete=models.CASCADE)
+    trader=models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Trades(models.Model):
@@ -24,7 +24,6 @@ class Trades(models.Model):
     class TradeDirection(models.IntegerChoices):
         BUY = 0, 'Buy'
         SELL = 1, 'Sell'
-    idTrade=models.IntegerField()
     strategy=models.ForeignKey(TradingStrategies, on_delete=models.CASCADE)
     activo=models.CharField(max_length=40)
     tradeDirection=models.IntegerField( choices=TradeDirection.choices)
